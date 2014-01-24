9fc273eaa3637bc635355b9e8e56af3c26ec77b2
Pei Qing 卿培 <qingpei@sansitech.com>
2014-01-24 16:12:00 +0800

    keep last 5 mail archives

--- bin/backup_thunderbird.sh
+++ bin/backup_thunderbird.sh
@@ -2,6 +2,10 @@
 ## Script to backup Thunderbird mail
 set -x
 x=`date +%y%m%d.%H%M`
-XZ_OPT=-6 tar cJf /tmp/thunderbird-${x}.tar.xz /home/qingpei/.thunderbird
+cd /home/qingpei
+XZ_OPT=-6 tar cJf /tmp/thunderbird-${x}.tar.xz .thunderbird
 mv /tmp/thunderbird-${x}.tar.xz /home/qingpei/Dropbox/Workspace/Sansi/backup/
+cd /home/qingpei/Dropbox/Workspace/Sansi/backup
+# keep only last 5 backups
+(ls -t | head -n 5;ls) | sort | uniq -u | xargs rm
 
